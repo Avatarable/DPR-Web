39f75b1b3ee11d2d96ef2f2a5fffd440d87530cc
Avatarable <38482898+Avatarable@users.noreply.github.com>
2022-02-25 22:44:36 +0100

    published app

---
+++ latest/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+let phoneField = document.querySelector("#PhoneNumber");
+
+phoneField.addEventListener('input', (e) => {
+    //return event.charCode != 8 && event.charCode == 0 || (event.charCode >= 48 && event.charCode <= 57);
+
+    if (e.shiftKey === true) {
+        if (e.which == 9) {
+            return true;
+        }
+        return false;
+    }
+    if (e.which > 57) {
+        return false;
+    }
+    if (e.which == 32) {
+        return false;
+    }
+    return true;
+})
+
+$(".alert-success").fadeTo(1000, 500).slideUp(500, function () {
+    $(".alert-success").slideUp(500);
+});
